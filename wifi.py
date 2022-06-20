6cdba6587cbb88ead75a139ce5b93defcf0a43da
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-06-20 21:32:10 +0700

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,34 +1,28 @@
 import subprocess
 import re
 
-commandLine = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+class wifi():
+    def getWifi():
+        commandLine = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+        profileNames = (re.findall("    All User Profile     : (.*)\r", commandLine))      
+        wifiList = list()
+        if len(profileNames) != 0:
+            for name in profileNames:
+                wifiProfile = dict()
+                profileInfo = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+                if re.search("    Security key           : Absent", profileInfo):
+                                Continue
 
-profileNames = (re.findall("    All User Profile     : (.*)\r", commandLine))
-               
-wifiList = list()
+                else:
+                    wifiProfile["ssid"] = name
+                    profileInfoPass = subprocess.run(["netsh", "wlan", "show", "profiles", name, "key=clear"], capture_output = True).stdout.decode()
+                    password = re.search("    Key Content            : (.*)\r", profileInfoPass)
+                    if password == None:
+                        wifiProfile["password"] = None
 
-if len(profileNames) != 0:
-    for name in profileNames:
-        wifiProfile = dict()
-
-        profileInfo = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
-
-        if re.search("    Security key           : Absent", profileInfo):
-                           Continue
-
-        else:
-            wifiProfile["ssid"] = name
-
-            profileInfoPass = subprocess.run(["netsh", "wlan", "show", "profiles", name, "key=clear"], capture_output = True).stdout.decode()
-
-            password = re.search("    Key Content            : (.*)\r", profileInfoPass)
-
-            if password == None:
-                wifiProfile["password"] = None
-
-            else:
-                wifiProfile["password"] = password[1]
-
-                wifiList.append(wifiProfile)
-for x in range(len(wifiList)):
-    print(wifiList[x])
+                    else:
+                        wifiProfile["password"] = password[1]
+                        wifiList.append(wifiProfile)
+        for x in range(len(wifiList)):
+            print(wifiList[x])
+wifi.getWifi()
