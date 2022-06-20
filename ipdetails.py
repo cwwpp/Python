66ce2b1b90473b34f2baf7c09319dcb23179f925
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-06-20 21:21:22 +0700

    Update ipdetails.py

--- ipdetails.py
+++ ipdetails.py
@@ -1,19 +1,22 @@
 import requests
 import pyautogui
 
-getIP = pyautogui.password('Enter IP: ')
+IP = pyautogui.password('ENTER IP:')
 
-response = requests.get('http://ip-api.com/json/' + getIP).json()
+class classIP():
+    def getIP(self, x):
+        response = requests.get('http://ip-api.com/json/' + x).json()
+        #print(response)
+        print('IP: ' + str(x))
+        print("Country: " + response['country'])
+        print('Country Code: ' + response['countryCode'])
+        print("City: " + response['city'])
+        print('Timezone: ' + response['timezone'])
+        print("Latitude: " + str(response['lat']))
+        print("Longitude: " + str(response['lon']))
+        print("ISP: " + str(response['isp']))
+        print('Organization : ' + response['org'])
+        print('AS: ' + response['as'])
 
-#print(response)
-
-print('IP: ' + str(getIP))
-print("Country: " + response['country'])
-print('Country Code: ' + response['countryCode'])
-print("City: " + response['city'])
-print('Timezone: ' + response['timezone'])
-print("Latitude: " + str(response['lat']))
-print("Longitude: " + str(response['lon']))
-print("ISP: " + str(response['isp']))
-print('Organization : ' + response['org'])
-print('AS: ' + response['as'])
+cIP = classIP()
+cIP.getIP(IP)
