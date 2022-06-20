0966a82d96b119b0d83f4a43e0111b0dca89bb54
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-06-20 20:46:05 +0700

    Update ipdetails.py

--- ipdetails.py
+++ ipdetails.py
@@ -9,14 +9,6 @@ response = requests.get('http://ip-api.com/json/' + getIP).json()
 
 #print(response)
 
-def tracertIP():
-    tracertLine = 'tracert -h 5 ' + str(getIP)
-    tracertCmd = os.system(tracertLine)
-
-def pingIP():
-    pingLine = 'ping ' + str(getIP)
-    pingCmd = os.system(pingLine)
-
 print('IP: ' + str(getIP))
 print("Country: " + response['country'])
 print('Country Code: ' + response['countryCode'])
@@ -27,36 +19,3 @@ print("Longitude: " + str(response['lon']))
 print("ISP: " + str(response['isp']))
 print('Organization : ' + response['org'])
 print('AS: ' + response['as'])
-
-#tracertIP()
-
-#def netstatIP():
-#    netstatLine = 'netstat -n ' + str(getIP)
-#    netstatCmd = os.system(netstatLine)
-
-#pingIP()
-
-
-
-#netstatIP()
-
-
-
-
-
-
-
-
-
-
-
-
-
-#101.53.52.5
-#113.173.78.40 (iceyzx)
-#42.118.57.248 (zalo)
-#173.244.36.41 (us ytber)
-#117.234.15.54 (indian ytber)
-
-# https://tools.keycdn.com/geo
-# https://iplogger.org/ | https://iplogger.org/logger/wpsf3fnCute1/
