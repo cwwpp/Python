e84a499c2ebc808c4683f73f4e0bc33c7ed71517
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-06-20 21:34:25 +0700

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,15 +1,15 @@
 import re
 import subprocess
 
-def ipGrab():
-    ipConfig = subprocess.run(['ipconfig'], capture_output = True).stdout.decode()
-    ipSearch = re.findall("IPv4 Address. . . . . . . . . . . : (.*)\r", ipConfig)
-    ipSearch2 = re.findall("Default Gateway . . . . . . . . . : (.*)\r", ipConfig)
-    personalIP = dict()
-    personalIP["Device IP Address"] = ipSearch
-    routerIP = dict()
-    routerIP["Router IP Address"] = ipSearch2
-    print(personalIP)
-    print(routerIP)
-
-ipGrab()
+class classIP():
+    def ipGrab():
+        ipConfig = subprocess.run(['ipconfig'], capture_output = True).stdout.decode()
+        ipSearch = re.findall("IPv4 Address. . . . . . . . . . . : (.*)\r", ipConfig)
+        ipSearch2 = re.findall("Default Gateway . . . . . . . . . : (.*)\r", ipConfig)
+        personalIP = dict()
+        personalIP["Device IP Address"] = ipSearch
+        routerIP = dict()
+        routerIP["Router IP Address"] = ipSearch2
+        print(personalIP)
+        print(routerIP)
+classIP.ipGrab()
