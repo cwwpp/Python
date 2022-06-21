bf41ac2c87481c4b2ca76dc68c84055dedae8ce2
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-06-21 12:42:31 +0700

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,12 @@
+import time, pyautogui
+
+numCLick = input('ENTER THE NUMBER OF CLICKS: ')
+
+class autoclicker():
+    def autoclick(self, x):
+        time.sleep(5)
+        for i in range(int(x)):
+            pyautogui.leftClick()
+            pyautogui.leftClick()
+auto = autoclicker()
+auto.autoclick(numCLick)
